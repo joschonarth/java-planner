1ec2f0d968de11a037faa24b3a4ced917d36133a
joschonarth <joschonarth@gmail.com>
2024-11-02 14:38:48 -0300

    Create activity entity

---
+++ src/main/java/com/joschonarth/planner/activities/Activity.java
@@ -0,0 +1,31 @@
+package com.joschonarth.planner.activities;
+
+import com.joschonarth.planner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "activities")
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "occurs_at", nullable = false)
+    private LocalDateTime occursAt;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+}
