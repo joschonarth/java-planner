f50e3f4a4fd455a09f865d22ac9abdf8309ca490
joschonarth <joschonarth@gmail.com>
2024-11-02 09:11:58 -0300

    Create participant entity

---
+++ src/main/java/com/joschonarth/planner/participant/Participant.java
@@ -0,0 +1,36 @@
+package com.joschonarth.planner.participant;
+
+import com.joschonarth.planner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "participants")
+public class Participant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private Boolean isConfirmed;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+}
