b5718cc760fd85f839d5c6270f3aec3ff7e701bb
joschonarth <joschonarth@gmail.com>
2024-11-02 18:24:12 -0300

    Create link entity

---
+++ src/main/java/com/joschonarth/planner/link/Link.java
@@ -0,0 +1,34 @@
+package com.joschonarth.planner.link;
+
+import com.joschonarth.planner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "links")
+public class Link {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(nullable = false)
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+
+}
