b9e2c9f00b7e1bd10f475bb6dfd5d80bde30d7ab
joschonarth <joschonarth@gmail.com>
2024-11-02 22:54:53 -0300

    Create date validation and exception for activity

--- src/main/java/com/joschonarth/planner/activity/ActivityService.java
+++ src/main/java/com/joschonarth/planner/activity/ActivityService.java
@@ -1,9 +1,11 @@
 package com.joschonarth.planner.activity;
 
+import com.joschonarth.planner.exception.InvalidDateException;
 import com.joschonarth.planner.trip.Trip;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,8 +16,9 @@ public class ActivityService {
     private ActivityRepository repository;
 
     public ActivityResponse registerActivity(ActivityRequestPayload payload, Trip trip) {
-        Activity newActivity = new Activity(payload.title(), payload.occurs_at(), trip);
+        validateActivityDate(LocalDateTime.parse(payload.occurs_at()), trip);
 
+        Activity newActivity = new Activity(payload.title(), payload.occurs_at(), trip);
         this.repository.save(newActivity);
 
         return new ActivityResponse(newActivity.getId());
@@ -23,5 +26,11 @@ public class ActivityService {
 
     public List<ActivityData> getAllActivitiesFromTrip(UUID tripId) {
         return this.repository.findByTripId(tripId).stream().map(activity -> new ActivityData(activity.getId(), activity.getTitle(), activity.getOccursAt())).toList();
+    }
+
+    private void validateActivityDate(LocalDateTime occursAt, Trip trip) {
+        if (occursAt.isBefore(trip.getStartsAt()) || occursAt.isAfter(trip.getEndsAt())) {
+            throw new InvalidDateException("The activity date must be within the trip period.");
+        }
     }
 }
--- src/main/java/com/joschonarth/planner/trip/TripController.java
+++ src/main/java/com/joschonarth/planner/trip/TripController.java
@@ -13,8 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
